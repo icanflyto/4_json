49fe997d989a46e475ba8fb74f944b1b634422cc
icanflyto <mad.idiots@gmail.com>
2017-07-21 20:56:17 +0000

    решение задачи

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,32 @@
 import json
+import sys
+
+from pprint import pprint
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, "r") as file:
+        return json.loads(file.read())
 
 
 def pretty_print_json(data):
-    pass
+    pprint(data, indent=4)
 
 
-if __name__ == '__main__':
-    pass
+if __name__ == "__main__":
+    if sys.argv < 2:
+        print("Usage: {0} <json_file>".format(sys.argv[0]))
+        sys.exit(1)
+
+    filepath = sys.args[1]
+
+    try:
+        data = load_data(filepath)
+    except IOError as e:
+        print("Open {0} error: {1}".format(filepath,e))
+        sys.exit(1)
+
+    try:
+        pretty_print_json(data)
+    except Exception as e:
+        print("Pretty printing error: {0}".format(e))
